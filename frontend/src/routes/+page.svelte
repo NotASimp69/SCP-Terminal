38687f9bd11c13a0104d7a4b32fcd180e92c7e78
fwefwefwefwef fwefwefwef <joey.jacobs0303@gmail.com>
2023-02-19 13:00:44 +0100

    first commit

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,55 @@
+<script>
+const onKeyPress = e => {
+    if (e.charCode === 13) addInput();
+};
+
+function addInput()
+{
+    var inputValue = document.getElementById("inputVal").value;
+
+    var konsoleDiv = document.getElementById("konsoleDiv")
+    var konsole = document.getElementById("konsole");
+    var li = document.createElement("li");
+
+    // inputValue    const cmdArgs = inputValue.split(' ');
+    // var command = cmdArgs[0];
+
+    switch(inputValue)
+    {
+    case "clear":
+        konsole.innerHTML = null;
+        li.appendChild(document.createTextNode(inputValue));
+
+        break;
+
+    default:
+    konsole.innerHTML += "Instruction: " +  inputValue + " not found.";     
+    break;
+    }
+    
+    konsole?.appendChild(li);
+
+
+    
+    konsoleDiv.scrollTop = konsoleDiv.scrollHeight;
+}
+</script>
+<div class="flex justify-center mt-3">
+    <img class="h-52 mt-5 opacity-60" src="https://upload.wikimedia.org/wikipedia/commons/2/2a/SCP_Foundation_Logo_v2_Fixed.png">
+</div>
+<div class="m-6">
+    <div class="flex mt-5 max-h-64 h-64 overflow-x-scroll mb-3" id="konsoleDiv">
+        <ul id="konsole">
+        </ul> 
+     </div>
+     <div>
+         <input id="inputVal" on:keypress={onKeyPress} type="text" placeholder="Type here" class="input input-bordered input-sm w-full hover:border-green-500 focus:border-green-500" style="color:lime;" />
+     </div>
+</div>
+<style>
+    #konsole
+    {
+        font-family: monospace;
+        color:rgb(0, 255, 0);    
+    }
+</style>
