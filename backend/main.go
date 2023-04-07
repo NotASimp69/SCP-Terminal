5b1fb8ea09951ea15dcf1c28d1c6c2be9075f3dd
Tim Arnold <frenggie@gmx.de>
2023-04-08 00:49:12 +0200

    Add: golang crawler

--- backend/main.go
+++ backend/main.go
@@ -1,18 +1,35 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 
-	workers "backend/main/modules"
+	workers "github.com/NotASimp69/SCP-Site/backend/modules"
 )
 
 func fetchPage(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
 	var query string = r.URL.Query().Get("query")
 
-	var fetched string = workers.Fetch(query)
-	fmt.Println(fetched)
+	if query == "" {
+		fetched := "Please provide a valid search query"
+		jsonResp, _ := json.Marshal(fetched)
+		w.Write(jsonResp)
+		return
+	} else {
+		fetched := workers.Fetch(query)
+		fmt.Println("Fetched ", fetched)
+
+		jsonResp, err := json.Marshal(fetched)
+		if err != nil {
+			log.Fatalf("Error happened in JSON marshal. Err: %s", err)
+		}
+		w.Write(jsonResp)
+		return
+	}
 }
 
 func main() {
--- backend/modules/workers.go
+++ backend/modules/workers.go
@@ -2,31 +2,86 @@ package workers
 
 import (
 	"fmt"
-	"time"
+	"log"
+	"strings"
+
+	"github.com/gocolly/colly"
 )
 
 type Entry struct {
-	Title   string `json:"Title"`
-	Class   string `json:"Class"`
-	Content string `json:"Content"`
+	Title       string
+	Class       string
+	Clearance   int
+	Procedures  string
+	Description string
+	Image       string
 }
 
-func scrap(query string) Entry {
+func scrape(query string) Entry {
 	var entry Entry
+	//entry = Entry{Title: "SCP-001", Class: "Euclid", Content: "Testing"}
 
-	entry = Entry{Title: "SCP-001", Class: "Euclid", Content: "Testing"}
+	c := colly.NewCollector(
+		colly.AllowedDomains("scp-wiki.wikidot.com"),
+	)
 
-	fmt.Println(entry)
+	c.OnHTML("div", func(e *colly.HTMLElement) {
+		//e.Request.Visit(e.Attr("href"))
+		if e.Attr("id") == "page-title" {
+			title, err := e.DOM.Html()
+			if err == nil {
+				title = strings.TrimPrefix(title, "\n")
+				title = strings.TrimSuffix(title, "\n")
+				title = strings.TrimSpace(title)
+				entry.Title = title
+			} else {
+				entry.Title = "Error"
+			}
+		} else if e.Attr("id") == "page-content" {
+			e.ForEach("p", func(i int, h *colly.HTMLElement) {
+				if strings.Contains(h.Text, "Object Class") {
+					entry.Class = strings.Replace(h.Text, "Object Class: ", "", -1)
+				} else if strings.Contains(h.Text, "Special Containment Procedures") {
+					entry.Procedures = strings.Replace(h.Text, "Special Containment Procedures: ", "", -1)
+				} else if strings.Contains(h.Text, "Description") {
+					entry.Description = strings.Replace(h.Text, "Description: ", "", -1)
+				}
+			})
+			e.ForEach("img", func(i int, h *colly.HTMLElement) {
+				fmt.Println(i)
+				fmt.Println(h)
+				if h.Attr("class") == "image" {
+					entry.Image = h.Attr("src")
+				}
+			})
+		}
+	})
+
+	c.OnError(func(_ *colly.Response, err error) {
+		log.Fatalln("Something went wrong while fetching the requested data...")
+	})
+
+	// c.OnRequest(func(r *colly.Request) {
+	// 	fmt.Println("Visiting", r.URL)
+	// })
+
+	c.Visit("https://scp-wiki.wikidot.com/" + query)
+
 	return entry
 }
 
-func Fetch(query string) string { // has to be uppercase to be public
+func Fetch(query string) Entry { // has to be uppercase to be public
 	fmt.Println("Initializing fetch...")
-	fmt.Println(query)
-	var data string
 
-	go scrap(query)
+	var data Entry
+	c := make(chan Entry)
 
-	time.Sleep(5 * time.Second)
+	go func() {
+		c <- scrape(query)
+	}()
+
+	data = <-c
+
+	//time.Sleep(5 * time.Second)
 	return data
 }
