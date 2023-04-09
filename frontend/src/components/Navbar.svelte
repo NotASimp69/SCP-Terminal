d6d7bc6d77edd22e965aff9e43ad7c8f70a9772b
Joey Jacobs <joey.jacobs0303@gmail.com>
2023-04-10 01:13:38 +0200

    among us

--- frontend/src/components/Navbar.svelte
+++ frontend/src/components/Navbar.svelte
@@ -1,6 +1,6 @@
 <div class="flex justify-between p-5 items-center bg-zinc-900 shadow-zinc-900 shadow-lg">
     <div class="">
-        <img src="https://img.logoipsum.com/288.svg">
+        <img class="w-1/6 " src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/SCP_Foundation_logo.svg/1200px-SCP_Foundation_logo.svg.png">
     </div>
     <div class="flex gap-5 items-center text-white text-lg">
         <a class="hover:text-red-600 duration-300" href="/privacy">Privacy Policy</a>
